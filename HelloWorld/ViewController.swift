f737147a200eb52393d5e06719f0ac3ba364e435
Fernando Paladini <fnpaladini@gmail.com>
2018-06-21 20:19:24 -0300

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 1Doc Tecnologia on 21/06/18.
+//  Copyright © 2018 Fernando Paladini. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
